07a5609e929c2c779e6473ce6154d7bff2d12172
Shahzain333 <shahzainkhannaizi1234@gmail.com>
2026-05-20 12:07:14 +0500

    Remove ImageKit File

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,47 @@
-import express from 'express';
+import express, { urlencoded } from 'express';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
 const app = express();
-app.use(cookieParser());
+
+// cors middleware for all request
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGINS,
+        credentials: true
+    })
+);
+
+// common middleware
+app.use(
+    express.json({limit: "16kb"}) // parses incoming JSON payloads in request body
+);
+app.use(
+    express.urlencoded({extended: true, limit: "16kb"}) // parses URL-encoded bodies with nested objects (extended: true)
+);
+app.use(
+    express.static("public") // serves static files from 'public' directory
+)
+app.use(cookieParser()); // parses Cookie header and populates req.cookies
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Portfolio Backend API");
+});
+
+// user routes
+// app.use("/api/v1/users", userRouter);
+//app.use("/api/v1/projects", projectRouter);
+//app.use("/api/v1/experience", experienceRouter);
+
+// testing middleware of auth
+// import { authMiddleware } from './middlewares/authMiddleware.js';
+// app.get("/test-middleware", authMiddleware, (req, res) => {
+//     res.send("Middleware tested successfully");
+// });
+
 
 // Error handler 
 app.use((err, req, res, next) => {
-    
     console.error(err); 
 
     const statusCode = err.statusCode || 500;
@@ -15,7 +50,6 @@ app.use((err, req, res, next) => {
         success: false,
         message: err.message || "Internal Server Error",
     });
-
 
 });
 
